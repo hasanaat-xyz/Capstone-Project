7f7a7f274efc28168fe6ba1410da386a04d1cb50
hasanaat <arsheema619@gmail.com>
2025-10-05 15:11:09 +0530

    changes made

--- Emotional-Intelligence-Assessment/client/src/QuizApp.jsx
+++ Emotional-Intelligence-Assessment/client/src/QuizApp.jsx
@@ -103,8 +103,7 @@ export default function QuizApp() {
     const response = await axios.post("http://localhost:5000/api/auth/login", formData);
     const currentUser = response.data.user;
     setUser(currentUser);
-
-    await axios.post("http://localhost:5000/api/quiz/result", {
+await axios.post("http://localhost:5000/api/quiz/result", {
       userId: currentUser._id,
       score: score,
       answers: userAnswers,
