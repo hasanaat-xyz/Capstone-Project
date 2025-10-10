d92ed5dbd1f44988e2673e39d179409e63a4c9aa
hasanaat <arsheema619@gmail.com>
2025-10-10 10:48:34 +0530

    auth.js

--- Emotional-Intelligence-Assessment/server/routes/auth.js
+++ Emotional-Intelligence-Assessment/server/routes/auth.js
@@ -10,7 +10,6 @@ router.post("/register", async (req, res) => {
 
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ msg: "User already exists" });
-
     user = new User({ name, email, password });
     await user.save();
 
