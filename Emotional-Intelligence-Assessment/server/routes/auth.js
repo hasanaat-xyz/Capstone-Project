53eb0aae4f400c3f26c95f08adde5be436b12e84
hasanaat <arsheema619@gmail.com>
2025-10-12 08:15:53 +0530

    routes update

--- Emotional-Intelligence-Assessment/server/routes/auth.js
+++ Emotional-Intelligence-Assessment/server/routes/auth.js
@@ -7,7 +7,6 @@ const router = express.Router();
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ msg: "User already exists" });
     user = new User({ name, email, password });
