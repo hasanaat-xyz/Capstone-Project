27de4854cbe5d743f72732fe0dedc2e1d9584cc7
hasanaat <arsheema619@gmail.com>
2025-10-14 07:51:09 +0530

    handlelogin-signup

--- Emotional-Intelligence-Assessment/client/src/App.jsx
+++ Emotional-Intelligence-Assessment/client/src/App.jsx
@@ -6,6 +6,7 @@ import LevelUpQuiz from "./LevelUpQuiz";
 
 function App() {
   const [user, setUser] = useState(null);
+
   // ✅ Load user from localStorage when app loads
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
@@ -38,5 +39,4 @@ function App() {
     </BrowserRouter>
   );
 }
-
 export default App;
