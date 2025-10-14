94e10bdb45fdc4da9264d3bd48742c1ab1f46125
hasanaat <arsheema619@gmail.com>
2025-10-14 09:40:38 +0530

    mongoose-schema

--- Emotional-Intelligence-Assessment/server/models/QuizResult.js
+++ Emotional-Intelligence-Assessment/server/models/QuizResult.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const quizResultSchema = new mongoose.Schema({  
-  
+
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   score: { type: Number, required: true },
   total: { type: Number, required: true },
   answers: [{ questionId: String, selectedOption: Number }],
   timePerQuestion: [{ type: Number }], 
   date: { type: Date, default: Date.now },
+  
 });
 
 export default mongoose.model("QuizResult", quizResultSchema);
