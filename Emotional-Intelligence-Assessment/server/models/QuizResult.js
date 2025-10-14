350aa36fbb152822412eaa398aa2c20710501404
hasanaat <arsheema619@gmail.com>
2025-10-14 08:07:22 +0530

    schmea updated

--- Emotional-Intelligence-Assessment/server/models/QuizResult.js
+++ Emotional-Intelligence-Assessment/server/models/QuizResult.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const quizResultSchema = new mongoose.Schema({
+  
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   score: { type: Number, required: true },
   total: { type: Number, required: true },
