9e2e5bfa6802c0480429231a4bcf39ee4cc7807d
hasanaat <arsheema619@gmail.com>
2025-10-17 11:35:54 +0530

    setFinished

--- Emotional-Intelligence-Assessment/client/src/levelUpQuiz3.jsx
+++ Emotional-Intelligence-Assessment/client/src/levelUpQuiz3.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import levelUpData3 from "./levelUpData3";
 
 export default function LevelUpQuiz3() {
-  
+
   const [currentQ, setCurrentQ] = useState(0);
   const [score, setScore] = useState(0);
   const [finished, setFinished] = useState(false);
@@ -16,7 +16,6 @@ export default function LevelUpQuiz3() {
       setFinished(true);
     }
   };
-
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-indigo-200 via-purple-200 to-pink-200 flex items-center justify-center">
       <div className="bg-white rounded-3xl shadow-2xl px-8 py-10 text-center w-full max-w-lg">
