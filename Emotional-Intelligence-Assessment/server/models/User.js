e931a0468467b2dfa73fcf3a2fd3da4e4b7a7015
hasanaat <arsheema619@gmail.com>
2025-10-18 16:10:02 +0530

    updates

--- Emotional-Intelligence-Assessment/server/models/User.js
+++ Emotional-Intelligence-Assessment/server/models/User.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+
   name: String,
   email: { type: String, unique: true },
   password: String, // (hash later with bcrypt)
   createdAt: { type: Date, default: Date.now },
+  
 });
 export default mongoose.model("User", userSchema);
