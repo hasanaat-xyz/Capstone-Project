612279ade50eadab864703ee39c52858e605c8b8
hasanaat <arsheema619@gmail.com>
2025-10-19 15:41:29 +0530

    level3

--- Emotional-Intelligence-Assessment/client/src/levelUpQuiz3.jsx
+++ Emotional-Intelligence-Assessment/client/src/levelUpQuiz3.jsx
@@ -44,8 +44,7 @@ export default function LevelUpQuiz3() {
             </p>
           </>
         ) : (
-          <div>
-            
+          <div>   
             <h2 className="text-2xl font-bold text-gray-800 mb-4">
               🎯 Level 3 Completed!
             </h2>
