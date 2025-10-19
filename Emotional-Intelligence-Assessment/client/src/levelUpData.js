76c8c56c422d4da40eb20e15eb0e3d3e4e30d945
hasanaat <arsheema619@gmail.com>
2025-10-19 15:46:18 +0530

    levelupdata

--- Emotional-Intelligence-Assessment/client/src/levelUpData.js
+++ Emotional-Intelligence-Assessment/client/src/levelUpData.js
@@ -12,7 +12,6 @@ const levelUpData = [
     answer: 1,
   },
   { //😌 Self-Regulation
-    
     question: "2. You’re angry at a teammate in a group project. What do you do first?",
     options: [
       "Send an angry email at midnight.",
