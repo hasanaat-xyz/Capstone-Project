481a43aab008523533edba1a952e19250a63fb87
hasanaat <arsheema619@gmail.com>
2025-10-20 13:36:43 +0530

    navigate

--- Emotional-Intelligence-Assessment/client/src/LandingPage.jsx
+++ Emotional-Intelligence-Assessment/client/src/LandingPage.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
   const navigate = useNavigate();
-
   // WhyCard Component
   function WhyCard({ item }) {
     const [open, setOpen] = useState(false);
