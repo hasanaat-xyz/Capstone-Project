bfd0614abdcbd501cc03f122f02d38771331e4ac
hasanaat <arsheema619@gmail.com>
2025-10-20 21:27:47 +0530

    setCurrentQ

--- Emotional-Intelligence-Assessment/client/src/LevelUpQuiz3.jsx
+++ Emotional-Intelligence-Assessment/client/src/LevelUpQuiz3.jsx
@@ -12,17 +12,17 @@ export default function LevelUpQuiz({ currentUser, level }) {
   const [timeStart, setTimeStart] = useState(Date.now());
 
   // Reset timer on each question
+
   useEffect(() => setTimeStart(Date.now()), [currentQ]);
 
   const handleSelect = async (i) => {
-    
+
     const timeSpent = Math.floor((Date.now() - timeStart) / 1000);
     setQuestionTimes((prev) => [...prev, timeSpent]);
     setUserAnswers((prev) => [...prev, i]);
     const isCorrect = i === levelUpData3[currentQ].answer;
     const newScore = score + (isCorrect ? 1 : 0);
     setScore(newScore);
-
     if (currentQ + 1 < levelUpData3.length) {
 
       setCurrentQ(currentQ + 1);
