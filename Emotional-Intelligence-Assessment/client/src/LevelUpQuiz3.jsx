991ae6d38da615ac54187b9e907eb0ed521b16e8
hasanaat <arsheema619@gmail.com>
2025-10-20 21:15:33 +0530

    setUserAnswers

--- Emotional-Intelligence-Assessment/client/src/LevelUpQuiz3.jsx
+++ Emotional-Intelligence-Assessment/client/src/LevelUpQuiz3.jsx
@@ -19,10 +19,9 @@ export default function LevelUpQuiz({ currentUser, level }) {
     const timeSpent = Math.floor((Date.now() - timeStart) / 1000);
     setQuestionTimes((prev) => [...prev, timeSpent]);
     setUserAnswers((prev) => [...prev, i]);
-    
+
     const isCorrect = i === levelUpData3[currentQ].answer;
     const newScore = score + (isCorrect ? 1 : 0);
-
     setScore(newScore);
 
     if (currentQ + 1 < levelUpData3.length) {
