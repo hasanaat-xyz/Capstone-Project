3b11414bda0620c023217a4fcbcc52415c2bb240
hasanaat <arsheema619@gmail.com>
2025-10-25 09:49:05 +0530

    components

--- Emotional-Intelligence-Assessment/client/src/components/EQassessment.jsx
+++ Emotional-Intelligence-Assessment/client/src/components/EQassessment.jsx
@@ -5,6 +5,7 @@ import LevelUpQuiz3 from "../components/LevelUpQuiz3";
 import EQReport from "../components/EQReport";
 
 export default function EQAssessment({ currentUser }) {
+  
   const [level, setLevel] = useState(1);
   const [results, setResults] = useState({ level1: null, level2: null, level3: null });
   const handleLevelComplete = (lvl, data) => {
@@ -12,6 +13,7 @@ export default function EQAssessment({ currentUser }) {
     if (lvl < 3) setLevel(lvl + 1);
     else setLevel(4);
   };
+
   return (
     <>
       {level === 1 && (
