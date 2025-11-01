bb686e9339cee26f60c932aca07bcc1a2152f3b3
hasanaat <arsheema619@gmail.com>
2025-11-01 09:22:22 +0530

    nov1

--- Emotional-Intelligence-Assessment/server/routes/auth.js
+++ Emotional-Intelligence-Assessment/server/routes/auth.js
@@ -74,7 +74,9 @@ router.post("/result", async (req, res) => {
     res.status(500).json({ msg: "Server error", error: err.message });
   }
 });
+
 // ✅ Get all results for a user
+
 router.get("/results/:userId", async (req, res) => {
   try {
     const results = await QuizResult.find({ userId: req.params.userId }).sort({ createdAt: -1 });
