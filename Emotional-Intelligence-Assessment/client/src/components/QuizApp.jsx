bf72685e7bac74296b4f39923171d738e68290f9
hasanaat <arsheema619@gmail.com>
2025-11-10 06:52:44 +0530

    setResults

--- Emotional-Intelligence-Assessment/client/src/components/QuizApp.jsx
+++ Emotional-Intelligence-Assessment/client/src/components/QuizApp.jsx
@@ -10,7 +10,7 @@ export default function Level1Quiz() {
   const [startTime, setStartTime] = useState(Date.now());
   const [showPopup, setShowPopup] = useState(false);
   const navigate = useNavigate();
-  
+
   const handleSelect = (index) => {
     const timeSpent = Math.floor((Date.now() - startTime) / 1000);
     const newAnswers = [...userAnswers, index];
@@ -30,6 +30,7 @@ export default function Level1Quiz() {
           score: newAnswers[i] === q.answer ? 1 : 0,
           timeSpent: newTimes[i],
         })),
+        
         level: 1,
         userAnswers: newAnswers,
         timePerQuestion: newTimes,
